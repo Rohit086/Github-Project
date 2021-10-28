0e7724d3157dfe0037e81a27fb095594066a6385
05 <05@DESKTOP-2O6D932>
2021-10-28 15:38:40 +0530

    Test java class created

---
+++ Github_Project/src/Testclass1.java
@@ -0,0 +1,25 @@
+
+public class Testclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		 int a=50;
+		   int   b=60;
+	          if (a<b)
+	          {
+	        	  System.out.println("a is smaller" +a);
+	        	  System.out.println(a);
+	          }
+	          else
+	          {
+	        	  System.out.println("b is smaller"+b);
+	        	   System.out.println(b);
+	        	 }
+	              System.out.println("End");
+		}
+	
+	}
+
+
